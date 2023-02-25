01ee257076a891fa39ad6d5b115a0c8316a1ae14
Crvstal8100 <99895952+Crvstal8100@users.noreply.github.com>
2023-02-25 15:05:49 +0100

    Update Behaviours.lua

--- Behaviours.lua
+++ Behaviours.lua
@@ -2,24 +2,4 @@ local Behaviours = {
   "Funny"
 }
 
-local Sentences = {
-  Funny = {
-	  Behaviour = "Funny",
-		Messages = {
-      "my honest reaction: ",
-      "naza doilet",
-      "fr",
-      "I love JuansIPad 😍🤤",
-      "So you see.. We're not just telling stories. We're changing lives.",
-      "🤓",
-      "SHADOW WIZARD MONEY GANG🔥🔥💯💯💯🗣🗣. WE LOVE CASTING SPELLS🔥🔥",
-      "Balls",
-      "💀",
-      "I like seeing big black men twerking while being covered in oil. 🤤🤤😍",
-      "Coems 🤑🤑",
-      "Ok men"
-    }
-  }
-}
-
-return Behaviours, Sentences
+return Behaviours
